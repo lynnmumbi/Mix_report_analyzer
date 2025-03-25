7cef1efb251b5cf69ff129cd5b5fd555d1dff27e
lynnmumbi <lynnmumbi8@gmail.com>
2025-03-25 14:27:39 +0300

    Update mix_app.py

--- mix_app.py
+++ mix_app.py
@@ -1,47 +1,5 @@
 import streamlit as st
 from Mix_stream import process_excel  # Import your function
-import base64
-
-def set_bg_image(image_file):
-    with open(image_file, "rb") as f:
-        encoded_string = base64.b64encode(f.read()).decode()
-    
-    bg_image = f"""
-    <style>
-    .stApp {{
-        background-image: url("data:image/jpg;base64,{encoded_string}");
-        background-size: cover;
-        background-position: center;
-    }}
-    
-    /* Set text color to white */
-    h1, h2, h3, h4, h5, h6, p, label {{
-        color: white !important;
-    }}
-
-    /* Style file uploader text */
-    .stFileUploader {{
-        color: white !important;
-    }}
-
-    </style>
-    """
-    st.markdown(bg_image, unsafe_allow_html=True)
-
-# Call the function
-set_bg_image("background.jfif")  # Ensure the image is in the same folder as your script
-
-st.markdown(
-    """
-    <style>
-        div[data-testid="stButton"] button {
-            color: black !important;  /* Change text color to black */
-            font-weight: bold;        /* Make text bold */
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 
 # Streamlit UI
